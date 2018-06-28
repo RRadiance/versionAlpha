fd2d76d97a30d8733eaf205dbd6fc44de395eb72
Matthew Huynh <mattrhuynh@gmail.com>
2018-06-28 02:41:01 -0400

    Cleans data from HTML doc
    Removes all unnecessary HTML tags, commas, escape characters, etc.
    Puts data into a dictionary

--- webscraper.py
+++ webscraper.py
@@ -5,18 +5,34 @@ from requests import get
 url = 'https://www.nasdaq.com/symbol/amzn' #AMZN is just an example
 response = get(url)
 
-#Create parse tree (BeautifulSoup Object)
+# Create parse tree (BeautifulSoup Object)
 soup = BeautifulSoup(response.text, 'html.parser')
 data = soup.find_all(class_= 'column span-1-of-2')
 
 items = []
-itemName = []
-itemValue = []
+# itemName = []
+# itemValue = []
 
-#Extract table rows
+# Extract table rows
 for i in range(len(data)):
-    items.append(data[i].find_all(class_='table-cell'))
+    items.extend(data[i].find_all(class_='table-cell'))
+# Cleans up data
+for i in range(len(items)):
+    # get_text strips the HTML tags
+    items[i] = items[i].get_text(strip = True).encode('ASCII', 'ignore').decode('utf-8')
+    # Gets rid of the extra ASCII characters, the
+    # 'ignore' keyword means any errors in the encoding
+    # will leave the character as a ''
+    # items[i] = items[i].encode('ASCII', 'ignore').decode('utf-8')
 
+# Puts data into a dictionary
+d = {}
+for i in range(0, len(items), 2):
+    d[items[i]] = items[i+1].replace(',','')
+    
+
+
+"""
 #Seperate name titles from values 
 for j in range(len(items)):
     for k in range(len(items[j])):
@@ -26,3 +42,4 @@ for j in range(len(items)):
             itemValue.append(lst[k])
 
 print(itemValue)
+"""
