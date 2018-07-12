a3536e69109b4db3686b9bdd3805ebfd98ea8a58
Matthew Huynh <mattrhuynh@gmail.com>
2018-07-12 17:17:14 -0400

    Changed into a class

--- webscraper.py
+++ webscraper.py
@@ -1,45 +1,40 @@
-#import html-parser
 from bs4 import BeautifulSoup
 from requests import get
 
-url = 'https://www.nasdaq.com/symbol/amzn' #AMZN is just an example
-response = get(url)
+class Webscraper:
 
-# Create parse tree (BeautifulSoup Object)
-soup = BeautifulSoup(response.text, 'html.parser')
-data = soup.find_all(class_= 'column span-1-of-2')
+    # Initialize the class
+    def __init__(self):
+        pass
 
-items = []
-# itemName = []
-# itemValue = []
+    # Retrieves and cleans the data from the NASDAQ Summary Page
+    def get_data_nasdaq_summary(self, ticker: str) -> dict:
+        url = 'https://www.nasdaq.com/symbol/'+ ticker
+        response = get(url)
+        # Create parse tree (BeautifulSoup Object)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        data = soup.find_all(class_= 'column span-1-of-2')
+        
+        items = []
+        # Extract table rows
+        for i in range(len(data)):
+            items.extend(data[i].find_all(class_='table-cell'))
 
-# Extract table rows
-for i in range(len(data)):
-    items.extend(data[i].find_all(class_='table-cell'))
-# Cleans up data
-for i in range(len(items)):
-    # get_text strips the HTML tags
-    items[i] = items[i].get_text(strip = True).encode('ASCII', 'ignore').decode('utf-8')
-    # Gets rid of the extra ASCII characters, the
-    # 'ignore' keyword means any errors in the encoding
-    # will leave the character as a ''
-    # items[i] = items[i].encode('ASCII', 'ignore').decode('utf-8')
+        # Cleans up data
+        for i in range(len(items)):
+            # get_text strips the HTML tags
+            items[i] = items[i].get_text(strip = True).encode('ASCII', 'ignore').decode('utf-8')
+            # Gets rid of the extra ASCII characters, the
+            # 'ignore' keyword means any errors in the encoding
+            # will leave the character as a ''
 
-# Puts data into a dictionary
-d = {}
-for i in range(0, len(items), 2):
-    d[items[i]] = items[i+1].replace(',','')
+        # Puts data into a dictionary
+        d = {}
+        for i in range(0, len(items), 2):
+            d[items[i]] = items[i+1].replace(',','')
 
+        return d
 
+    # Make a lot more get_data functions
 
-"""
-#Seperate name titles from values
-for j in range(len(items)):
-    for k in range(len(items[j])):
-        lst = items[j]
-        itemName.extend(lst[k].find_all('b'))
-        if k%2 != 0:
-            itemValue.append(lst[k])
-
-print(itemValue)
-"""
+    
